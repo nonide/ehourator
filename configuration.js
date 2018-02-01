245823ab0318f914a0bf195587eb7eaf222bbf3a
Pablo Hernández Nonide <pablo.hernandez@gigigo.com>
2018-02-01 10:52:14 +0100

    Default values in reconfiguration

--- configuration.js
+++ configuration.js
@@ -11,27 +11,36 @@ async function newConfig() {
     console.log("Let's configure your ehourator")
     console.log('-----------------------------------------')
 
+    let exists = await fs.exists(configPath)
+    let fileConfig
+    if (exists)
+        fileConfig = await getConfigObj()
+
     const response = await inquirer.prompt(
         [
             {
                 type: 'input',
                 name: 'username',
                 message: 'Enter your eHour username',
+                default: fileConfig && fileConfig.username
             },
             {
                 type: 'password',
                 name: 'password',
                 message: 'Enter your eHour password',
+                mask: '*',
             },
             {
                 type: 'input',
                 name: 'url',
                 message: 'Enter your eHour LOGIN URL',
+                default: fileConfig && fileConfig.url
             },
             {
                 type: 'input',
                 name: 'projectName',
                 message: 'Enter the project name',
+                default: fileConfig && fileConfig.projectName
             },
         ]
     )
@@ -44,13 +53,17 @@ async function newConfig() {
     await fs.writeFile(configPath, JSON.stringify(config))
 }
 
+async function getConfigObj() {
+    let fileConfig = await fs.readFile(configPath, 'utf8')
+    return JSON.parse(fileConfig)
+}
+
 async function getConfig() {
     let exists = await fs.exists(configPath)
     if (!exists)
         await newConfig()        
 
-    let fileConfig = await fs.readFile(configPath, 'utf8')
-    return JSON.parse(fileConfig)
+    return await getConfigObj()
 }
 
 module.exports.getConfig = getConfig
